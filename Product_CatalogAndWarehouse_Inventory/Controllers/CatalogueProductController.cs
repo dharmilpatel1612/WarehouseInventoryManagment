0160eeca510f46d6f55d81915a1cca55d5c22b46
pinkal <patelpinkal6010@gmail.com>
2023-04-12 10:12:35 +0530

    Add catalogue product page product dimension, product image-Done

--- Product_CatalogAndWarehouse_Inventory/Controllers/CatalogueProductController.cs
+++ Product_CatalogAndWarehouse_Inventory/Controllers/CatalogueProductController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Product_CatalogAndWarehouse_Inventory.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,81 @@ namespace Product_CatalogAndWarehouse_Inventory.Controllers
 {
     public class CatalogueProductController : Controller
     {
+        StringBuilder sb;
+        Dal obj_dal;
         // GET: CatalogueProduct
         public ActionResult CatalogueProduct()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult CatalogueProduct(CatalogueProductModel catalogue)
+        {
+            try 
+            {
+                obj_dal = new Dal();
+                sb = new StringBuilder();
+                sb.Append("select count(WarehouseSKU) from tblCatalogueProduct where WarehouseSKU='" + catalogue.WarehouseSKU + "'");
+                int Count = Convert.ToInt32(obj_dal.Get_SingleValue(sb.ToString()));
+                if (Count > 0)
+                {
+                    // Error message in red color
+                    ViewBag.color = "red";
+                    ViewBag.ErrorMessage = " Duplicate Warehouse SKU";
+                }
+                else
+                {
+                    if (catalogue.ProductImage != null)
+                    {
+                        var path = Server.MapPath("~/Uploads/");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        var imgfileName = Guid.NewGuid().ToString() + Path.GetExtension(catalogue.ProductImage.FileName);
+
+                        string filepath = Path.Combine(path, imgfileName);
+                        catalogue.ProductImage.SaveAs(filepath);
+                        sb.Clear();
+                        sb.Append("INSERT INTO tblCatalogueProduct ");
+                        sb.Append("(ProductName,WarehouseSKU,Description,HSCode,Width,Length,Height,MaterialComposition,VarsanyBag,Colour,");
+                        sb.Append("ProductWeight,BoxSize,BoxQuantity,Manufacturer,MailingBag,Size,ProductCost,ShippingCost,Commision,Duty,ProductImage) ");
+                        sb.Append("VALUES('" + catalogue.ProductName + "','" + catalogue.WarehouseSKU + "','" + catalogue.Description + "','" + catalogue.HSCode + "',");
+                        sb.Append("'" + catalogue.Width + "','" + catalogue.Length + "','" + catalogue.Height + "','" + catalogue.MaterialComposition + "',");
+                        sb.Append("'" + catalogue.VarsanyBag + "','" + catalogue.Colour + "','" + catalogue.ProductWeight + "','" + catalogue.BoxSize + "',");
+                        sb.Append("'" + catalogue.BoxQuantity + "','" + catalogue.Manufacturer + "','" + catalogue.MaillingBag + "','" + catalogue.Size + "',");
+                        sb.Append("'" + catalogue.ProductCost + "','" + catalogue.Shippingncost + "','" + catalogue.Commision + "','" + catalogue.Duty + "','" + imgfileName + "')");
+                        obj_dal.EXECUTE_DML(sb.ToString());
+                        //string conString = string.Empty;
+                        ViewBag.Message = " Data inserted Successfully";
+
+                    }
+                    else
+                    {
+
+                        sb.Clear();
+                        sb.Append("INSERT INTO tblCatalogueProduct ");
+                        sb.Append("(ProductName,WarehouseSKU,Description,HSCode,Width,Length,Height,MaterialComposition,VarsanyBag,Colour,");
+                        sb.Append("ProductWeight,BoxSize,BoxQuantity,Manufacturer,MailingBag,Size,ProductCost,ShippingCost,Commision,Duty) ");
+                        sb.Append("VALUES('" + catalogue.ProductName + "','" + catalogue.WarehouseSKU + "','" + catalogue.Description + "','" + catalogue.HSCode + "',");
+                        sb.Append("'" + catalogue.Width + "','" + catalogue.Length + "','" + catalogue.Height + "','" + catalogue.MaterialComposition + "',");
+                        sb.Append("'" + catalogue.VarsanyBag + "','" + catalogue.Colour + "','" + catalogue.ProductWeight + "','" + catalogue.BoxSize + "',");
+                        sb.Append("'" + catalogue.BoxQuantity + "','" + catalogue.Manufacturer + "','" + catalogue.MaillingBag + "','" + catalogue.Size + "',");
+                        sb.Append("'" + catalogue.ProductCost + "','" + catalogue.Shippingncost + "','" + catalogue.Commision + "','" + catalogue.Duty + "')");
+                        obj_dal.EXECUTE_DML(sb.ToString());
+                        ViewBag.Message = " Data inserted Successfully";
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                ViewBag.color = "red";
+                ViewBag.ErrorMessage = ex.Message;
+
+            }
+            return View();
+
+        }
+       
     }
 }
--- Product_CatalogAndWarehouse_Inventory/Models/CatalogueProductModel.cs
+++ Product_CatalogAndWarehouse_Inventory/Models/CatalogueProductModel.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Web;
 
 namespace Product_CatalogAndWarehouse_Inventory.Models
 {
+    public class FileExtensionValidate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                HttpPostedFileWrapper file = (HttpPostedFileWrapper)value;
+                string extension = Path.GetExtension(file.FileName);
+                if (extension != ".jpg" & extension != ".png")
+                {
+                    return new ValidationResult("only .jpg and .png file allow");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
     public class CatalogueProductModel
     {
         [Required(ErrorMessage = "Required")]
@@ -14,27 +31,29 @@ namespace Product_CatalogAndWarehouse_Inventory.Models
         public string WarehouseSKU { get; set; }
         public string Description { get; set; }
         public string HSCode { get; set; }
-        public int Width { get; set; }
-        public int Length { get; set; }
-        public int Height { get; set; }
+        public decimal Width { get; set; }
+        public decimal Length { get; set; }
+        public decimal Height { get; set; }
         public string MaterialComposition { get; set; }
         public string VarsanyBag { get; set; }
         public string Colour { get; set; }
-        public int ProductWeight { get; set; }
-        public int BoxSize { get; set; }
+        public decimal ProductWeight { get; set; }
+        public decimal BoxSize { get; set; }
         public int BoxQuantity { get; set; }
-        public int Manufacturer { get; set; }
-        public int MaillingBag { get; set; }
-        public int Size { get; set; }
+        public string Manufacturer { get; set; }
+        public string MaillingBag { get; set; }
+        public string Size { get; set; }
         [Required(ErrorMessage = "Required")]
-        public int ProductCost { get; set; }
-        public int Shippingncost { get; set; }
-        public int Commision { get; set; }
-        public int Duty { get; set; }
-        public int ChineseYuan { get; set; }
-        public int USDollar { get; set; }
-        public int Euros { get; set; }
-        public int TotalCost { get; set; }
+        public decimal ProductCost { get; set; }
+        public decimal Shippingncost { get; set; }
+        public decimal Commision { get; set; }
+        public decimal Duty { get; set; }
+        public decimal ChineseYuan { get; set; }
+        public decimal USDollar { get; set; }
+        public decimal Euros { get; set; }
+        public decimal TotalCost { get; set; }
+        [FileExtensionValidate]
+        public HttpPostedFileBase ProductImage { get; set; }
 
     }
 }
